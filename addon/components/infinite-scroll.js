9a6744cafee08e83ca2d04b66acc1c89ef3c9a01
Dylan Foster <dylan947@gmail.com>
2016-07-20 07:19:31 -0700

    feat(component): clean up all the things

--- addon/components/infinite-scroll.js
+++ addon/components/infinite-scroll.js
@@ -1,113 +1,102 @@
-import Em from 'ember';
+import Ember from 'ember';
 
-var $window = Em.$(window),
-    $document = Em.$(document),
-    bind = Em.run.bind;
+const { $, get, getWithDefault, on, run, set } = Ember;
+const $window = $(window);
+const $document = $(document);
 
-var EPSILON = 150;
+const EPSILON = 150;
 
-export default Em.Component.extend({
+export default Ember.Component.extend({
   action: 'fetchMore',
   epsilon: EPSILON,
   isFetching: false,
   hasMore: null,
   content: null,
-  scrollElement: null, // Defaults to '$window'
-  debounce: null, // Defaults to false
+  scrollElement: null,
+  debounce: null,
 
-  setup: function() {
-    // console.log( 'eis setup' );
-    /** Setup some default selectors */
-    this.safeSet( '$scrollElement', $window );
-    this.safeSet( 'scrollEvent', 'scroll.' + this.elementId );
-    /** Override these if we're not using default selectors */
-    if( typeof this.get( 'scrollElement' ) == 'string' ){
-      this.safeSet( '$scrollElement', Em.$(this.scrollElement + ':first') );
-      this.safeSet( 'scrollEvent', 'scroll' );
-    }
-    /** Setup default debouncing if we need it */
-    if( typeof this.get( 'debounce' ) != 'boolean' && typeof this.get( 'debounce' ) != 'number' ){
-      this.safeSet( 'debounce', false );
-    }
-    /** Generate a random ID for our scroll event */
-    var scrollHandlerId = 'scrollHandler_' + Math.ceil( Math.random() * 100000 )
-    // console.log( 'eis listening to event: ' + scrollHandlerId );
-    this.safeSet( 'scrollHandlerId', scrollHandlerId );
-    /** Listen for the scroll event on our element */
-    this.get( '$scrollElement' ).on( this.get( 'scrollEvent' ), bind( this, this.didScroll ) );
-  }.on('didInsertElement'),
+  setup: on('didInsertElement', function () {
+    this.safeSet('$scrollElement', $window);
+    this.safeSet('scrollEvent', `scroll.${this.elementId}`);
 
-  teardown: function() {
-    // console.log( 'eis teardown: ' + this.get( 'scrollHandlerId' ) );
-    /** Monitoring scrolling */
-    this.get( '$scrollElement' ).off(this.get('scrollEvent'));
-    /** If we have a timeout, clear it */
-    if( typeof this.get( 'timeout' ) != 'undefined' ){
+    if (typeof get(this, 'scrollElement') === 'string') {
+      this.safeSet('$scrollElement', $(`${this.scrollElement}:first`));
+      this.safeSet('scrollEvent', 'scroll');
+    }
+
+    if (typeof get(this, 'debounce') !== 'boolean' && typeof get(this, 'debounce') !== 'number' ) {
+      this.safeSet('debounce', false);
+    }
+
+    const scrollHandlerId = `scrollHandler_${Math.ceil(Math.random() * 100000)}`;
+
+    this.safeSet('scrollHandlerId', scrollHandlerId);
+    get(this, '$scrollElement').on(get(this, 'scrollEvent'), run.bind(this, this.didScroll));
+  }),
+
+  teardown: on('willDestroyElement', function () {
+    get(this, '$scrollElement').off(get(this, 'scrollEvent'));
+
+    if (get(this, 'timeout')) {
       clearTimeout( this.get( 'timeout' ) );
     }
-  }.on('willDestroyElement'),
+  }),
 
-  didScroll: function() {
-    // console.log( 'eis didScroll' );
-    if( !this.get( 'debounce' ) ){
-      /** If we aren't debouncing, handle this directly */
-      this.handleScroll.apply( this );
-    }else{
-      /** So we need to debounce, use set/clear timeout methods */
-      if( typeof this.get( 'timeout' ) != 'undefined' ){
-        clearTimeout( this.get( 'timeout' ) );
-      }
-      /** Ok, so now set the timeout for the 'handleScroll' event */
-      var timer = typeof this.get( 'debounce' ) == 'number' ? this.get( 'debounce' ) : 250;
-      /** Now actually do it, binding to 'this' object */
-      this.safeSet( 'timeout', setTimeout( this.handleScroll.bind( this ), timer ) );
+  didScroll() {
+    if (!get(this, 'debounce')) {
+      this.handleScroll();
+    } else {
+      clearTimeout( this.get( 'timeout' ) );
+
+      const timer = typeof get(this, 'debounce' ) === 'number' ? get(this, 'debounce') : 250;
+      this.safeSet('timeout', setTimeout(this.handleScroll.bind(this), timer));
     }
   },
 
-  handleScroll: function(){
-    // console.log( 'eis handleScroll' );
-    if (!this.get('isFetching') && this.get('hasMore') && this.isNearBottom()) {
+  handleScroll(){
+    if (!get(this, 'isFetching') && get(this, 'hasMore') && this.isNearBottom()) {
       this.safeSet('isFetching', true);
-      this.sendAction('action', bind(this, this.handleFetch));
+      this.sendAction('action', run.bind(this, this.handleFetch));
     }
   },
 
-  handleFetch: function(promise) {
-    var success = bind(this, this.fetchDidSucceed),
-        failure = bind(this, this.fetchDidFail);
+  handleFetch(promise) {
+    const success = run.bind(this, this.fetchDidSucceed);
+    const failure = run.bind(this, this.fetchDidFail);
 
-    promise.then(success, failure);
+    return promise.then(success, failure);
   },
 
-  fetchDidSucceed: function(response) {
-    var content = this.get('content'),
-        newContent = Em.getWithDefault(response, 'content', response);
+  fetchDidSucceed(response) {
+    const content = get(this, 'content');
+    const newContent = getWithDefault(response, 'content', response);
 
     this.safeSet('isFetching', false);
     if (content) { content.pushObjects(newContent); }
   },
 
-  fetchDidFail: function() {
+  fetchDidFail() {
     this.safeSet('isFetching', false);
   },
 
-  isNearBottom: function() {
-    if( this.$scrollElement == $window ){
+  isNearBottom() {
+    if (this.$scrollElement === $window ) {
       /** Keep our legacy functionality if we're listening to the window scroll event */
-      var viewPortTop = $document.scrollTop(),
-          bottomTop = ($document.height() - $window.height());
-      return viewPortTop && (bottomTop - viewPortTop) < this.get('epsilon');
-    } else{
+      const viewPortTop = $document.scrollTop();
+      const bottomTop = ($document.height() - $window.height());
+
+      return viewPortTop && (bottomTop - viewPortTop) < get(this, 'epsilon');
+    } else {
       /**
        * We're going to use the scroll element to calculate the height, if we're not using the default functionality
        * from: http://stackoverflow.com/questions/6271237/detecting-when-user-scrolls-to-bottom-of-div-with-jquery
        * retreived: 20150205
        */
-      return this.get( '$scrollElement' ).scrollTop() + this.get( '$scrollElement' ).innerHeight() >= this.get( '$scrollElement' )[0].scrollHeight - this.get('epsilon');
+      return get(this, '$scrollElement').scrollTop() + get(this, '$scrollElement').innerHeight() >= get(this, '$scrollElement')[0].scrollHeight - get(this, 'epsilon');
     }
   },
 
-  safeSet: function(key, value) {
-    if (!this.isDestroyed && !this.isDestroying) { this.set(key, value); }
+  safeSet(key, value) {
+    if (!this.isDestroyed && !this.isDestroying) { set(this, key, value); }
   }
 });
